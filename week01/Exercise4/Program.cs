3c7fd834bcdeb21d82be20fe76e027106f7b378f
DumpsterDom <dominictg926@gmail.com>
2025-10-28 20:50:37 -0400

    exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber;
+        do
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+
+            // Safety parse 
+            if (int.TryParse(userResponse, out userNumber))
+            {
+                if (userNumber == 0)
+                    break;
+
+                numbers.Add(userNumber);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        } while (true);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        // Sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Average
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Max
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
