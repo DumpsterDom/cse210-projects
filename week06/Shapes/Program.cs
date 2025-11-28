f2dc4d74b62dc29e8f6404a3523dd164876c8e2e
DumpsterDom <dominictg926@gmail.com>
2025-11-27 22:57:01 -0500

    Shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // list of Shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 6));
+        shapes.Add(new Circle("Green", 3));
+        shapes.Add(new Square("Yellow", 2.5));
+        shapes.Add(new Rectangle("Purple", 3, 8));
+
+        // Iterate the list
+        Console.WriteLine("=== All Shapes ===\n");
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"{color} shape -> Area: {area:F2}");
+        }
     }
 }
